be5dbc9e4fdc9f3d2d259f1e32d9e4bc3012e63a
Evgeniy Kudashov <kudashovea@gmail.com>
2023-11-04 21:23:37 +0300

    added api description

--- src/main/java/ru/ekudashov/controller/FrequencyEvaluatorController.java
+++ src/main/java/ru/ekudashov/controller/FrequencyEvaluatorController.java
@@ -1,5 +1,12 @@
 package ru.ekudashov.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.tags.Tags;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -8,6 +15,10 @@ import ru.ekudashov.service.FrequencyService;
 
 import java.util.Map;
 
+
+@Tags(value = {
+        @Tag(name = "Frequency Evaluator", description = "provides api about evaluating frequency")
+})
 @RestController
 @RequestMapping("/evaluators/frequency")
 public class FrequencyEvaluatorController {
@@ -19,6 +30,17 @@ public class FrequencyEvaluatorController {
         this.frequencyService = textFormatService;
     }
 
+    @Operation(description = "returns an array of symbols in descending frequency order",
+            parameters = @Parameter(name = "text",
+                    description = "text to be evaluated",
+                    example = "aaabbc"),
+            responses = @ApiResponse(responseCode = "200",
+                    content = @Content(examples =
+                    @ExampleObject(value ="{\n" +
+                            "    \"a\": 3,\n" +
+                            "    \"b\": 2,\n" +
+                            "    \"c\": 1\n" +
+                            "}"))))
     @GetMapping(params = "text",
             produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
